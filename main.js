0867f3686d3569e456fa4c0223626778fe49925b
Laura-D-Andrews <ldandrews88@gmail.com>
2023-04-21 16:10:18 -0400

    added event listeners

--- main.js
+++ main.js
@@ -2,14 +2,38 @@
 let displayScreen = document.querySelector('#display')
 console.log(displayScreen)
 
-let buttonClear = document.querySelector('#clear')
+let buttonClear = document.querySelector('#buttonClear')
 console.log(buttonClear)
 
-let buttonNumbers = document.querySelectorAll(".buttonNum")
-console.log(buttonNumbers)
-
-let buttonOperators = document.querySelectorAll(".buttonOp")
-console.log(buttonOperators)
+let buttons = document.querySelectorAll(".button")
+console.log(buttons)
 
 let buttonEqual = document.querySelector("#buttonEq")
 console.log(buttonEqual)
+
+gridCard.addEventListener('click', (event) => {
+    let newDisplay = document.createTextNode(${event}) 
+
+    console.log(${event});
+})
+
+
+buttonClear.addEventListener("click", (event) => {
+    console.log(event.target.innerText); 
+    displayScreen.innerText = event.target.innerText; 
+})
+// displays in console
+
+for (let button of buttons) {
+    button.addEventListener("click", (event) => {
+        console.log(event.target.innerText); 
+        displayScreen.innerText = event.target.innerText;
+    })
+}
+// displays in console 
+
+buttonEqual.addEventListener("click", (event) => {
+    console.log(event.target.innerText);
+    displayScreen.innerText = event.target.innerText; 
+})
+// displays in console
